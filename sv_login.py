01ddc6e66e6cf817efe024531c8f5b9b5fc7d82f
NicolasRiosM <69985870+NicolasRiosM@users.noreply.github.com>
2021-07-11 16:21:41 -0400

    Update sv_login.py

--- sv_login.py
+++ sv_login.py
@@ -13,42 +13,42 @@ s.send(bytes('00010sinitlogin','utf-8'))
 print("Ingresando a la cuenta de usuario")
 recibido=s.recv(4096)
 
-if s.recv(4096)!=b'00012sinitOKlogin':
-    
-    while True:
-        datos = s.recv(4096)
-        print(datos)
-        if datos.decode('utf-8').find('login'):
-            datos = datos[10:]
-            target = datos.decode()
-            data = target.split()
-            email = data[0]
-            password = data[1]
-            val = 0
-            consulta = "select email, pass from usuario"
-            respuesta = consultar(consulta)
-            enchash = hashlib.md5(password.encode())
-            pass2=enchash.hexdigest()
-            print("hola")
-            for i in respuesta:
-                mail = i[0]
-                passw = i[1] 
-                print(passw)
-                print(pass2)
-                if mail == email and passw==pass2:
-                    val=1
-                    print("Ha ingresado con éxito a su cuenta")
-                    respuesta2='login'+mail+passw
-                    
-                    break
-                else:
-                    respuesta2 = 'login' + "no_existe_usuario"
-                    break
-            else:
-                pass
-    print(respuesta2)
-    temp=llenado(len(respuesta2))  
-    s.sendall(bytes(temp+respuesta2,'utf-8'))
 
-    if (val!=1):
-        print("Contraseña incorrecta")
+    
+while True:
+    datos = s.recv(4096)
+    print(datos)
+    if datos.decode('utf-8').find('login'):
+        datos = datos[10:]
+        target = datos.decode()
+        data = target.split()
+        email = data[0]
+        password = data[1]
+        val = 0
+        consulta = "select email, pass from usuario"
+        respuesta = consultar(consulta)
+        enchash = hashlib.md5(password.encode())
+        pass2=enchash.hexdigest()
+        print("hola")
+        for i in respuesta:
+            mail = i[0]
+            passw = i[1] 
+            print(passw)
+            print(pass2)
+            if mail == email and passw==pass2:
+                val=1
+                print("Ha ingresado con éxito a su cuenta")
+                respuesta2='login'+mail+passw
+                
+                break
+            else:
+                respuesta2 = 'login' + "no_existe_usuario"
+                break
+        else:
+            pass
+print(respuesta2)
+temp=llenado(len(respuesta2))  
+s.sendall(bytes(temp+respuesta2,'utf-8'))
+
+if (val!=1):
+    print("Contraseña incorrecta")
