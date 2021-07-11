d59384febd563baf534215f0c9ec67dfa87d6bf1
NicolasRiosM <69985870+NicolasRiosM@users.noreply.github.com>
2021-07-10 22:58:50 -0400

    Update sv_login.py

--- sv_login.py
+++ sv_login.py
@@ -19,7 +19,7 @@ while True:
         datos = datos[10:]
         target = datos.decode()
         data = target.split()          
-    consulta = "select email, pass from usuario"
+    consulta = f"select email, pass from usuario"
     respuesta = consultar(consulta)
 #--------------------------------------------------------------#
     i=0
