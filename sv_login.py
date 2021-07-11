3fdf72bf6956410c53e7c20f757fc50f87de90ff
NicolasRiosM <69985870+NicolasRiosM@users.noreply.github.com>
2021-07-11 16:21:19 -0400

    Update sv_login.py

--- sv_login.py
+++ sv_login.py
@@ -11,7 +11,8 @@ s.send(bytes('00010sinitlogin','utf-8'))
 
 #def recibir(sock, addr):
 print("Ingresando a la cuenta de usuario")
-print(s.recv(4096))
+recibido=s.recv(4096)
+
 if s.recv(4096)!=b'00012sinitOKlogin':
     
     while True:
@@ -23,26 +24,28 @@ if s.recv(4096)!=b'00012sinitOKlogin':
             data = target.split()
             email = data[0]
             password = data[1]
-        val = 0
-        consulta = "select email, pass from usuario"
-        respuesta = consultar(consulta)
-        enchash = hashlib.md5(password.encode())
-        pass2=enchash.hexdigest()
-        print("hola")
-        for i in respuesta:
-            mail = i[0]
-            passw = i[1] 
-            print(passw)
-            print(pass2)
-            if mail == email and passw==pass2:
-                val=1
-                print("Ha ingresado con éxito a su cuenta")
-                respuesta2='login'+mail+passw
-                
-                break
+            val = 0
+            consulta = "select email, pass from usuario"
+            respuesta = consultar(consulta)
+            enchash = hashlib.md5(password.encode())
+            pass2=enchash.hexdigest()
+            print("hola")
+            for i in respuesta:
+                mail = i[0]
+                passw = i[1] 
+                print(passw)
+                print(pass2)
+                if mail == email and passw==pass2:
+                    val=1
+                    print("Ha ingresado con éxito a su cuenta")
+                    respuesta2='login'+mail+passw
+                    
+                    break
+                else:
+                    respuesta2 = 'login' + "no_existe_usuario"
+                    break
             else:
-                respuesta2 = 'login' + "no_existe_usuario"
-                break
+                pass
     print(respuesta2)
     temp=llenado(len(respuesta2))  
     s.sendall(bytes(temp+respuesta2,'utf-8'))
